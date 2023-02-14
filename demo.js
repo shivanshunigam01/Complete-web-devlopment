f7806d92fe3b2e96120c21e6e2f8c05bad110d90
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-14 12:42:48 +0530

    14-02-2023

---
+++ demo.js
@@ -0,0 +1,31 @@
+  msg.js 
+
+function msg(){   
+
+alert("Hello Javascript");   
+
+}   
+
+ 
+
+<html>   
+
+<head>   
+
+<script type="text/javascript" src="msg.js"></script>   
+
+</head>   
+
+<body>   
+
+<p>Welcome to JavaScript</p>   
+
+<form>   
+
+<input type="button" value="click" onclick="msg()"/>   
+
+</form>   
+
+</body>   
+
+</html>   
